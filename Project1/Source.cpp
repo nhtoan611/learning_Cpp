0eb6288bc9bc65cdee64566aa5c7dd552ef78a16
nhtoan611 <nhtoan611@gmail.com>
2017-04-16 11:02:30 +0700

    first commit

---
+++ Project1/Source.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+void xinchao();
+class demo {
+public:
+	demo() {
+		age = 3;
+	}
+	demo(int tuoi) {
+		age = tuoi;
+	}
+public:
+	void setAge(int tuoi) {
+		age = tuoi;
+	}
+	int getAge() {
+		return age;
+	}
+private:
+	int age;
+};
+void main() {
+	/*	xinchao();
+	int a = 3;
+	cout << a << endl;
+	int b;
+	b = 2;
+	cout << b << endl;
+	*/
+	demo x;
+	cout << x.getAge() << endl;
+	demo y(5);
+	cout << y.getAge() << endl;
+	system("pause");
+}
+
+void xinchao() {
+	cout << "Xin chao cac ban" << endl;
+}
